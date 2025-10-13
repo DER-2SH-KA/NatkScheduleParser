0e5fb3f3e8fecad4182c905aa81df1146b724fee
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-10-13 16:48:23 +0700

    feat: add load properties into Main

--- src/main/java/ru/der2shka/Main.java
+++ src/main/java/ru/der2shka/Main.java
@@ -1,7 +1,39 @@
 package ru.der2shka;
 
+import ru.der2shka.exception.SettingsPropertiesIsEmptyException;
+import ru.der2shka.util.properties.PropertiesReader;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Properties;
+
 public class Main {
+    private static Properties properties = new Properties();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        loadSettingsProperties();
+    }
+
+    private static void loadSettingsProperties() {
+        try {
+            properties = PropertiesReader.getSettingsProperties();
+        }
+        catch (URISyntaxException ex) {
+            System.err.println("URI syntax of settings file exception");
+            ex.printStackTrace();
+        }
+        catch (SettingsPropertiesIsEmptyException ex) {
+            System.err.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+        catch (FileNotFoundException ex) {
+            System.err.println("File of settings not found.");
+            ex.printStackTrace();
+        }
+        catch (IOException ex) {
+            System.err.println("Failed to load content from settings file");
+            ex.printStackTrace();
+        }
     }
 }
