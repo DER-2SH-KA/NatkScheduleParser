6a34d7015e9b8af9c30c5a5ddbc6c025bc9a06ee
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-10-14 00:54:12 +0700

    refactor: add toString() into Class.java

--- src/main/java/ru/der2shka/model/Class.java
+++ src/main/java/ru/der2shka/model/Class.java
@@ -3,6 +3,17 @@ package ru.der2shka.model;
 import lombok.Data;
 
 @Data
-public sealed abstract class Class permits OneLineClass, MultiLineClass {
+public class Class {
     private String date;
+    private Subject subject;
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Class: %s%nDate: %s%nSubject: %s%n",
+                this.getClass().getName(),
+                this.getDate(),
+                this.getSubject().toString()
+        );
+    }
 }
