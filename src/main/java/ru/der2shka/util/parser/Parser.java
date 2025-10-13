d7827779ab146e6db395f6839059ced112a6f6a9
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-10-13 18:37:05 +0700

    feat: add Parser

---
+++ src/main/java/ru/der2shka/util/parser/Parser.java
@@ -0,0 +1,57 @@
+package ru.der2shka.util.parser;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Properties;
+
+public class Parser {
+    private static Parser instance;
+
+    @Getter
+    @Setter
+    private Integer timeOut = 0;
+
+    private static final String userAgent =
+            "Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6";
+
+    public static Parser getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new Parser();
+        }
+
+        return instance;
+    }
+
+    public Document getDocument(String url) throws IOException, NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sc = SSLContext.getInstance("TLS");
+        sc.init(null, new TrustManager[] {
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+                }
+        }, new SecureRandom());
+
+
+        return Jsoup.connect(url)
+                .ignoreHttpErrors(true)
+                .userAgent(userAgent)
+                .sslContext( sc)
+                .timeout(timeOut)
+                .referrer("https://ya.ru")
+                .get();
+    }
+}
