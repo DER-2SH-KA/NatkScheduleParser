6da7426c64d1c727c0d3d00be77d49a068cd8e17
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-10-13 18:37:25 +0700

    feat: add parse group NATK page

--- src/main/java/ru/der2shka/Main.java
+++ src/main/java/ru/der2shka/Main.java
@@ -1,21 +1,33 @@
 package ru.der2shka;
 
+import org.jsoup.nodes.Document;
 import ru.der2shka.exception.SettingsPropertiesIsEmptyException;
+import ru.der2shka.util.parser.Parser;
 import ru.der2shka.util.properties.PropertiesReader;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
 import java.util.Properties;
 
 public class Main {
     private static Properties properties = new Properties();
+    private static final Parser parser = Parser.getInstance();
+
+    private static final String natkGroupsUrl = "https://natk.ru/stud-grad/schedule";
 
     public static void main(String[] args) {
         loadSettingsProperties();
 
         System.out.println(properties.getProperty("hello.world"));
         System.out.println(System.getenv("TELEGRAM_TOKEN"));
+
+        Optional<Document> document = parseDocument(natkGroupsUrl);
+
+        document.ifPresent(System.out::println);
     }
 
     private static void loadSettingsProperties() {
@@ -38,5 +50,25 @@ public class Main {
             System.err.println("Failed to load content from settings file");
             ex.printStackTrace();
         }
+    }
+
+    private static Optional<Document> parseDocument(String url) {
+        Optional<Document> document = Optional.empty();
+
+        try {
+            document = Optional.of(parser.getDocument(url));
+        }
+        catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
+        }
+        catch (KeyManagementException ex) {
+            ex.printStackTrace();
+        }
+        catch (IOException ex) {
+            System.err.println("IOException when connect to " + natkGroupsUrl);
+            ex.printStackTrace();
+        }
+
+        return document;
     }
 }
