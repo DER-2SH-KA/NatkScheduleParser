dc51aec44630c6cfa9ef229410edfa4acc201c45
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-10-17 19:05:56 +0700

    refactor: add in ClassEntity new columns

--- src/main/java/ru/der2shka/entity/ClassEntity.java
+++ src/main/java/ru/der2shka/entity/ClassEntity.java
@@ -8,4 +8,22 @@ public class ClassEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "date")
+    private String date;
+
+    @Column(name = "seq_num")
+    private Integer seqNum;
+
+    @Column(name = "time_period", length = 20)
+    private String timePeriod;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    @Column(name = "teacher_fio")
+    private String teacherFio;
+
+    @Column(name = "address", length = 255)
+    private String address;
 }
