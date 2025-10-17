95a4692ecc47bcb8e0c7d16ee987fc2cd0f9795e
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-10-17 20:30:04 +0700

    feat: add loading DotEnv on start in Main

--- src/main/java/ru/der2shka/Main.java
+++ src/main/java/ru/der2shka/Main.java
@@ -1,22 +1,22 @@
 package ru.der2shka;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import ru.der2shka.util.database.hibernate.HibernateUtil;
+import ru.der2shka.util.dotenv.DotEnvReader;
 import ru.der2shka.util.properties.PropertiesReader;
 import ru.der2shka.util.telegrambot.NatkTelegramBot;
 
+import java.util.Optional;
 import java.util.Properties;
 
 public class Main {
 
     public static void main(String[] args) {
-        Properties db = PropertiesReader.loadProperties(PropertiesReader.databaseSettingsFileName);
-
-        System.out.println("\n\nDB url: " + db.getProperty("database.url"));
-        System.out.println("DB username: " + db.getProperty("database.username"));
-        System.out.println("DB password: " + db.getProperty("database.password") + "\n\n");
+        DotEnvReader.getEnv();
 
         getInstanceOfSessionFactory();
+
         registerBot();
     }
 
