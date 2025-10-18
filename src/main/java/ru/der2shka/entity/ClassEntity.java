fe1684bc38848bfe55285e8f0b3413be30200f9f
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-10-18 21:32:05 +0700

    refactor: ClassEntity override hashcode, equals and toString methods, add equalsByFieldsExcludeId method

--- src/main/java/ru/der2shka/entity/ClassEntity.java
+++ src/main/java/ru/der2shka/entity/ClassEntity.java
@@ -2,6 +2,8 @@ package ru.der2shka.entity;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "study_classes")
 public class ClassEntity {
@@ -26,4 +28,49 @@ public class ClassEntity {
 
     @Column(name = "address", length = 255)
     private String address;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    /**
+     * Compare this object and entity fields excluding id filed on equals.
+     * @param entity {@link ClassEntity} object;
+     * @return are equals this object and entity.
+     * **/
+    public boolean equalsByFieldsExcludeId(ClassEntity entity) {
+        return this.date.equals(entity.date) &&
+                this.seqNum == entity.seqNum &&
+                this.timePeriod.equals(entity.timePeriod) &&
+                this.name.equals(entity.name) &&
+                this.teacherFio.equals(entity.teacherFio) &&
+                this.address.equals(entity.address);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+
+        if (o == this) return true;
+
+        if (!(o instanceof ClassEntity oce)) return false;
+
+        return this.hashCode() == oce.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(String.format("ID: %d%n", this.id));
+        sb.append(String.format("Date: %s%n", this.date));
+        sb.append(String.format("SequenceNum: %d%n: ", this.seqNum));
+        sb.append(String.format("Time period: %s%n", this.timePeriod));
+        sb.append(String.format("Name: %s%n", this.name));
+        sb.append(String.format("Teacher: %s%n", this.teacherFio));
+        sb.append(String.format("Address: %s%n", this.address));
+
+        return sb.toString();
+    }
 }
