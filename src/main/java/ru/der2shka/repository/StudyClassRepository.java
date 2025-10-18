e9b06134a272279e1845e7f0975b05d5de699129
DER-2SH-KA <KurumiForWeb@yandex.ru>
2025-10-18 21:35:08 +0700

    feat: add StudyClassRepository

---
+++ src/main/java/ru/der2shka/repository/StudyClassRepository.java
@@ -0,0 +1,130 @@
+package ru.der2shka.repository;
+
+import jakarta.persistence.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.jetbrains.annotations.NotNull;
+import ru.der2shka.entity.ClassEntity;
+import ru.der2shka.util.database.hibernate.HibernateUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class StudyClassRepository {
+
+    /**
+     * Save entity in database.
+     * @param entity entity to save.
+     * @return {@link ClassEntity} as {@link Optional} object.
+     * @throws NullPointerException if entity param is null.
+     * @throws RuntimeException if transaction is failed (with rollback).
+     * **/
+    public Optional<ClassEntity> save(@NotNull ClassEntity entity) {
+
+        Objects.requireNonNull(entity, "Entity must be not null!");
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        try {
+            Transaction transaction =  session.beginTransaction();
+
+            ClassEntity savedEntity = session.merge(entity);
+
+            transaction.commit();
+
+            return Optional.ofNullable(savedEntity);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+
+            if (session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+
+            throw new RuntimeException("Error saving entity", ex);
+        }
+        finally {
+            session.close();
+        }
+    }
+
+    /**
+     * Find entities in database.
+     * **/
+    public List<ClassEntity> findAll() {
+
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+            final String request = "SELECT ce FROM ClassEntity ce";
+
+            TypedQuery<ClassEntity> query = session
+                    .createQuery(request, ClassEntity.class);
+
+            return query.getResultList();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return new ArrayList<ClassEntity>();
+    }
+
+    /**
+     * Find entity in database by ID.
+     * @param id entity's ID.
+     * @throws NullPointerException if id param is null.
+     * **/
+    public Optional<ClassEntity> findById(@NotNull Long id) {
+
+        Objects.requireNonNull(id, "Entities' ID must be not null!");
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+            ClassEntity foundedEntity = session.find(ClassEntity.class, id);
+
+            return Optional.ofNullable(foundedEntity);
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return Optional.empty();
+    }
+
+    /**
+     * Delete entity from database.
+     * @param entity entity to save.
+     * @throws NullPointerException if entity param is null.
+     * @throws RuntimeException if transaction is failed (with rollback).
+     * **/
+    public void delete(@NotNull ClassEntity entity) {
+
+        Objects.requireNonNull(entity, "Entity must be not null!");
+
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+
+        try {
+
+            Transaction transaction = session.beginTransaction();
+
+            session.remove(entity);
+
+            transaction.commit();
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+
+            if (session.getTransaction().isActive()) {
+                session.getTransaction().rollback();
+            }
+        }
+        finally {
+            session.close();
+        }
+    }
+}
